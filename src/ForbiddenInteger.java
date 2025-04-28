df0fac3e06eefb8c489b93180dfaf4d6206f882a
smranjanmishra <soumyarmishra065@gmail.com>
2025-04-28 10:54:02 +0530

    added the fixed codes

---
+++ src/ForbiddenInteger.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class ForbiddenInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+            long x = sc.nextLong();
+
+            if (x != 1) {
+                System.out.println("YES");
+                System.out.println(n);
+                for (int i = 1; i <= n; i++) {
+                    System.out.print(1 + " ");
+                }
+                System.out.println();
+            } else {
+                if (k == 1 || (k == 2 && n % 2 == 1)) {
+                    System.out.println("NO");
+                } else {
+                    System.out.println("YES");
+                    if (n % 2 == 0) {
+                        System.out.println(n / 2);
+                        for (int i = 1; i <= n / 2; i++) {
+                            System.out.print(2 + " ");
+                        }
+                        System.out.println();
+                    } else {
+                        System.out.println((n - 3) / 2 + 1);
+                        for (int i = 1; i <= (n - 3) / 2; i++) {
+                            System.out.print(2 + " ");
+                        }
+                        System.out.println(3);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+// tc - O(n) = O(100)
+// sc - O(1)
---
+++ src/GrasshopperonaLine.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GrasshopperonaLine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long x = sc.nextLong();
+            long k = sc.nextLong();
+            if (x % k == 0) {
+                System.out.println(2);
+                System.out.println(1 + " " + (x - 1));
+            } else {
+                System.out.println(1);
+                System.out.println(x);
+            }
+        }
+    }
+}
+
+// tc - O(1)
+// sc - O(1)
