282d39a6319a8e198572815856d6d4a42b8bba07
smranjanmishra <soumyarmishra065@gmail.com>
2025-04-30 09:14:50 +0530

    Added the code of Prepend and Append

---
+++ src/PrependandAppend.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrependandAppend {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            String s = sc.next();
+            long ans = n;
+            long left = 0, right = n - 1;
+            while (left <= right) {
+                if (s.charAt((int) left) != s.charAt((int) right))
+                    ans -= 2;
+                else
+                    break;
+                left++;
+                right--;
+            }
+            System.out.println(ans);
+        }
+    }
+}
+
+// tc - O(n) = O(2000)
+// sc - O(n) = O(2000)
