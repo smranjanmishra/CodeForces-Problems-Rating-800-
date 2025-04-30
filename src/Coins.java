da750d4a6d1c8600c9d9e7513dbfd02de26a9a0c
smranjanmishra <soumyarmishra065@gmail.com>
2025-04-30 09:06:39 +0530

    Added the code of coins

---
+++ src/Coins.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Coins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+            if (n % 2 == 0 || (n - k) % 2 == 0) {
+                // we took y = 0 or y = 1
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
+
+// tc - O(1)
+// sc - O(1)
