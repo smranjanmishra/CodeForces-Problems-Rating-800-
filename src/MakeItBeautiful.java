90f051b8e2373ee6eb4edbf1b953bee61b60e9b3
smranjanmishra <soumyarmishra065@gmail.com>
2025-04-30 09:19:30 +0530

    Added the code of Make it Beautiful

---
+++ src/MakeItBeautiful.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class MakeItBeautiful {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++)
+                a[i] = sc.nextLong();
+
+            Arrays.sort(a);
+            long maxNumber = a[n - 1];
+            long minNumber = a[0];
+
+            if (maxNumber == minNumber) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+                System.out.print(maxNumber + " ");
+                for (int i = 0; i < n - 1; i++)
+                    System.out.print(a[i] + " ");
+                System.out.println();
+            }
+        }
+    }
+}
+
+// tc : O(nlogn) + O(n) = O(nlogn) = O(50) = O(50*log2(50)) = O(50*6) = O(300)
+// sc : O(n) = O(50)
